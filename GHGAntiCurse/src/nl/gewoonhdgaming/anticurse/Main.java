adad81c8329377a96458e8f4e1178f7badaa2a6f
Rens Mulder <mulderrens@outlook.com>
2017-03-05 08:45:03 +0100

    Made the plugin base; TODO: plugin.yml, bug fixing!

--- GHGAntiCurse/src/nl/gewoonhdgaming/anticurse/Main.java
+++ GHGAntiCurse/src/nl/gewoonhdgaming/anticurse/Main.java
@@ -1,7 +1,25 @@
 package nl.gewoonhdgaming.anticurse;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import nl.gewoonhdgaming.anticurse.events.PlayerChatEvent;
 
 public class Main extends JavaPlugin {
 
+	public void onEnable() {
+		ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+		
+		console.sendMessage(ChatColor.GREEN + "Het AntiCurse systeem wordt geladen!");
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+		Bukkit.getServer().getPluginManager().registerEvents(new PlayerChatEvent(), this);
+		console.sendMessage(ChatColor.AQUA + "Het AntiCurse systeem is succesvol geladen!");
+	}
+	
+	public void onDisable() {
+	
+	}
 }
